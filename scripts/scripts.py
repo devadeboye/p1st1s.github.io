fa4804bb2b9bdc3aee6c827d39103a98be6ce5e2
p1st1s <emmanueladeboye2017@gmail.com>
2020-05-29 16:07:08 +0100

    - Add function that choose which profile pics to display based on   screen size - change background of landingpage

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -1,8 +1,31 @@
-from browser import document, bind
+from browser import document, bind, html
 
+
+def profile_pics_selector():
+    """
+    choose appropriate profile pics based on screen size
+    """
+    # use project element to detect if device is mobile
+    mobile_view = document["project_small_display"].style.display
+    pp_wrapper = document["profile_pics_cont"]
+    small_img = "./img/about_pics_small.JPG"
+    normal_img = "./img/about_pics.JPG"
+    if mobile_view != "none":
+        pp_wrapper <= html.IMG(
+            src=small_img,
+            alt="picture of emmanuel"
+        )
+    else:
+        pp_wrapper <= html.IMG(
+            src=normal_img,
+            alt="picture of emmanuel"
+        )
 
 @bind("#menu_icon", "click")
 def show_menu(event):
+    """
+    show menu item on small display
+    """
     menu = document["mobile_nav"]
     menu_button = document["menu_icon"]
     close_button = document["close_icon"]
@@ -13,10 +36,17 @@ def show_menu(event):
         close_button.classList.add("show")
         menu.classList.remove("hide")
         menu.classList.add("show")
+    #if menu.style.display == "none":
+    #    menu_button.style.display = "hide"
+    #    close_button.style.display = "block"
+    #    menu.style.display = "block"
 
 
 @bind("#close_icon", "click")
 def hide_menu(event):
+    """
+    hide menu item on small display
+    """
     menu = document["mobile_nav"]
     menu_button = document["menu_icon"]
     close_button = document["close_icon"]
@@ -32,3 +62,7 @@ def hide_menu(event):
 @bind("#show_email", "click")
 def show_email(event):
     document["show_email"].textContent = "adeboye3a@gmail.com"
+
+
+# choose appropriate profile pics based on screen size
+profile_pics_selector()
